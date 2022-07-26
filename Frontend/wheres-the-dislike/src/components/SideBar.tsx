59a16d1ef332f135c44e914853fef80a680b0dbc
fsit869 <fsit869@aucklanduni.ac.nz>
2022-07-26 23:19:10 +1200

    Implemented more of sidebar

--- Frontend/wheres-the-dislike/src/components/SideBar.tsx
+++ Frontend/wheres-the-dislike/src/components/SideBar.tsx
@@ -1,12 +1,32 @@
-import {List, ListItem, ListItemIcon, ListItemText} from "@mui/material";
+import {Divider, List, ListItem, ListItemIcon, ListItemText} from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import {useState} from "react";
 
 export const SideBar = (): JSX.Element => {
+    const [aboutDialogueOpen, setAboutDialogueOpen] = useState(false);
     return (
-        <div>
+        <div style={{width: 250}}>
             {/*Delete all button*/}
             <List>
-                <ListItem button onClick={e => console.log("Hey")}>
-                    <ListItemText primary="Delete All Notes"/>
+                <ListItem button onClick={e => console.log("Hey")} >
+                    <ListItemIcon><HomeIcon/></ListItemIcon>
+                    <ListItemText primary="Dashboard"/>
+                </ListItem>
+            </List>
+
+            <Divider/>
+            <List>
+                <ListItem button onClick={e => alert("Ever heard of Youtubed Vanced?\nI haven't")} >
+                    <ListItemIcon><VisibilityOffIcon/></ListItemIcon>
+                    <ListItemText primary="Secret"/>
+                </ListItem>
+            </List>
+            <List>
+                <ListItem button onClick={e => console.log("Hey")} >
+                    <ListItemIcon><InfoIcon/></ListItemIcon>
+                    <ListItemText primary="About"/>
                 </ListItem>
             </List>
         </div>
