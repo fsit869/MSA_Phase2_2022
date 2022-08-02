00c2abb347cafa6e3695123beba53f47204a69a7
fsit869 <fsit869@aucklanduni.ac.nz>
2022-08-02 13:36:49 +1200

    Demo different app setting profiles

--- Backend/WebApplication1/WebApplication1/Program.cs
+++ Backend/WebApplication1/WebApplication1/Program.cs
@@ -1,7 +1,13 @@
+using System.Configuration;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using NuGet.Configuration;
 using WebApplication1.model;
+
+using Microsoft.Extensions.Configuration;
+using WebApplication1;
+
 
 var builder = WebApplication.CreateBuilder(args);
 // https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-6.0&tabs=visual-studio
@@ -33,6 +39,18 @@ builder.Services.AddSwaggerGen(options =>
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
 
+
+
+// DEMO DiFFERENT PROFILES https://www.daveabrock.com/2021/01/19/config-top-level-programs/
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var region = config["Region"];
+var key = config["Key"];
+Console.WriteLine($"APP PROFILE DEMO: Starting server in {region}!");
+Console.WriteLine($"Using Key {key}!");
 
 var app = builder.Build();
 
